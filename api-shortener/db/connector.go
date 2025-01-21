ccf98e720f48624210e58ac3c264e09f1f765daf
HidenYT <buharev2009@gmail.com>
2025-01-21 21:42:25 +0300

    Removed debug gorm settings

--- api-shortener/db/connector.go
+++ api-shortener/db/connector.go
@@ -2,14 +2,10 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"time"
 
 	"github.com/caarlos0/env/v11"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type DBCreationSettings struct {
@@ -32,17 +28,7 @@ func (settings *DBCreationSettings) GetConnectionString() string {
 }
 
 func CreateDB(settings *DBCreationSettings) (*gorm.DB, error) {
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      true,        // Don't include params in the SQL log
-			Colorful:                  false,       // Disable color
-		},
-	)
-	return gorm.Open(postgres.Open(settings.GetConnectionString()), &gorm.Config{Logger: newLogger})
+	return gorm.Open(postgres.Open(settings.GetConnectionString()), &gorm.Config{})
 }
 
 func NewDBConnectionSettings() *DBCreationSettings {
